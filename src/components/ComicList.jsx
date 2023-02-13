7140ce191916f100c773fdf36061ee0335ae279c
alvaroveloz <alvaro.veloz@gmail.com>
2023-02-13 17:51:11 -0500

    CADO-LA - Added episodes format

--- src/components/ComicList.jsx
+++ src/components/ComicList.jsx
@@ -5,6 +5,12 @@ export const ComicList = ( { characters } ) => {
 
   const hasResults = characters?.length > 0;
 
+
+  const shortEpisode = (episodes = [] ) => {
+    const arrEpisodes = episodes.slice(0, 4).map(episode => episode.split('/').pop() ).join(', ');
+    return (arrEpisodes.length > 4 ) ? `${arrEpisodes}...` : arrEpisodes
+  }
+
   
   return (
     <div className='page'>
@@ -18,11 +24,15 @@ export const ComicList = ( { characters } ) => {
               <p>{character.Name}</p>
               <p>{character.Gender}</p>
               <p>{character.Species}</p>
-              <p>{character.Episode}</p>
-              {character.Status === 'unknown' ? null :( <button> { character.Status}</button>) }
+              <p>{shortEpisode(character.Episode)}</p>
+              {character.Status === 'unknown' ? null : (
+                <button> {character.Status}</button>
+              )}
             </div>
           </div>
         ))}
     </div>
   );
 }
+
+
