1115993dd1e765d792407f3d470f275d1bc75fed
alvaroveloz <alvaro.veloz@gmail.com>
2023-02-14 17:58:28 -0500

    CADO-LA - Using tailwindcss instead of vanilla css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+// Vanilla CSS
+// import './index.css'; 
+import './styles.css'  // Tailwind CSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
